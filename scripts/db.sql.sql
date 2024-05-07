886409abcd0e57b202ccf9f5035f31fc01d0a995
GerardoNR <22620048@tlaxiaco.tecnm.mx>
2024-05-06 21:18:59 -0600

    corregí unos errores
    corregí unos errores

--- scripts/db.sql.sql
+++ scripts/db.sql.sql
@@ -27,7 +27,7 @@ PRIMARY KEY (id_concesionaria)
 );
 
 CREATE TABLE Cliente (
-id_cliente VARCHAR(30) NOT NULL,
+id_cliente VARCHAR(50) NOT NULL,
 nombre VARCHAR(30) NOT NULL,
 telefono VARCHAR(30) NOT NULL,
 domicilio VARCHAR(30) NOT NULL,
@@ -56,82 +56,62 @@ FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
 FOREIGN KEY (id_empleado) REFERENCES empleado (id_empleado)
 );
 
-INSERT INTO consecionaria(Dirección,Correo,Telefono,rol)
-values ('Direccion 1', 'email@examplee.com', '3344546', 'usuario'),
-       ('Direccion 2', 'email2@ecample.com', '43576346', 'admin'),
-	   ('Direccion 3', 'email2@egvervv.com', '435564', 'usuario'),
-	   ('Direccion 4', 'email2@revhvtv.com', '43574363', 'admin'),
-	   ('Direccion 5', 'email2@ebjyjhe.com', '435437', 'usuario'),
-	   ('Direccion 6', 'email2@ytrbtds.com', '47878435', 'admin'),
-	   ('Direccion 7', 'email2@3454hvr.com', '4367745', 'usuario'),
-	   ('Direccion 8', 'email2@ballers.com', '74365546', 'admin'),
-	   ('Direccion 9', 'email2@erytbju.com', '44364477', 'usuario'),
-	   ('Direccion 10', 'email2@56un6g.com', '654742757', 'admin');
+INSERT INTO consecionaria(id_consecionaria, dirección,Correo,Telefono,rol)
+values ('100' ,'Direccion 1', 'email@examplee.com', '3344546', 'usuario'),
+       ('200' ,'Direccion 2', 'email2@ecample.com', '43576346', 'admin'),
+	   ('300' ,'Direccion 3', 'email2@egvervv.com', '435564', 'usuario'),
+	   ('400' ,'Direccion 4', 'email2@revhvtv.com', '43574363', 'admin'),
+	   ('500' ,'Direccion 5', 'email2@ebjyjhe.com', '435437', 'usuario'),
+	   ('600' ,'Direccion 6', 'email2@ytrbtds.com', '47878435', 'admin'),
+	   ('700' ,'Direccion 7', 'email2@3454hvr.com', '4367745', 'usuario'),
+	   ('800' ,'Direccion 8', 'email2@ballers.com', '74365546', 'admin'),
+	   ('900' ,'Direccion 9', 'email2@erytbju.com', '44364477', 'usuario'),
+	   ('1000' ,'Direccion 10', 'email2@56un6g.com', '654742757', 'admin');
 
- INSERT INTO  Auto (
-    marca,
-    color, 
-    año, 
-    tipo,
-    precio,
-    combustible,
-    )
-    values ('ferrari' ,'blanco' , '2000', 'deportivo' ,'200,000' , 'gasolina'),
-    ('toyota' ,'azul' , '1990', 'deportivo' ,'500,000' , 'gasolina'),
-    ('ford' ,'rojo' , '2001', 'deportivo' ,'350,000' , 'gasolina'),
-    ('nissan' ,'negro' , '1985', 'deportivo' ,'480,000' , 'gasolina'),
-    ('Chevrolet' ,'verde' , '1995', 'deportivo' ,'400,000' , 'gasolina'),
-    ('nissan' ,'cafe' , '1999', 'deportivo' ,'380,000' , 'gasolina'),
-    ('Volkswagen' ,'negro' , '2000', 'deportivo' ,'41,000' , 'gasolina'),
-    ('honda' ,'blanco' , '2001', 'deportivo' ,'440,000' , 'gasolina'),
-    ('audi' ,'amarillo' , '2005', 'deportivo' ,'320,000' , 'gasolina'),
-    ('mazda' ,'negro' , '2003', 'deportivo' ,'320,000' , 'gAsolina');
+ INSERT INTO  Auto (id_auto, marca, color, año, tipo, precio, combustible)
+    values ('001' , 'ferrari ' ,'blanco' , '2000', 'deportivo' ,'200,000' , 'gasolina'),
+    ('002' ,'toyota' ,'azul' , '1990', 'deportivo' ,'500,000' , 'gasolina'),
+    ('003' ,'ford' ,'rojo' , '2001', 'deportivo' ,'350,000' , 'gasolina'),
+    ('004' ,'nissan' ,'negro' , '1985', 'deportivo' ,'480,000' , 'gasolina'),
+    ('005' ,'Chevrolet' ,'verde' , '1995', 'deportivo' ,'400,000' , 'gasolina'),
+    ('006' ,'nissan' ,'cafe' , '1999', 'deportivo' ,'380,000' , 'gasolina'),
+    ('007' ,'Volkswagen' ,'negro' , '2000', 'deportivo' ,'41,000' , 'gasolina'),
+    ('008' ,'honda' ,'blanco' , '2001', 'deportivo' ,'440,000' , 'gasolina'),
+    ('009' ,'audi' ,'amarillo' , '2005', 'deportivo' ,'320,000' , 'gasolina'),
+    ('010' ,'mazda' ,'negro' , '2003', 'deportivo' ,'320,000' , 'gAsolina');
     
-    INSERT INTO  consecionaria (
-    nombre,
-    ubicacion,
-    telefono,
-    tipo_auto,
-    )
-    values ('tienda1' ,'tlaxiaco' , '9531145698', 'deportivo'),
-    ('tienda2' ,'oaxaca' , '9531145698', 'deportivo'),
-    ('tienda3' ,'mexico' , '9531178890', 'deportivo'),
-    ('tienda4' ,'puebla' , '9531146112', 'deportivo'),
-    ('tienda5' ,'tijuana' , '9531143709', 'deportivo'),
-    ('tienda6' ,'putla' , '9531180098', 'deportivo'),
-    ('tienda7' ,'jalisco' , '9531146458', 'deportivo'),
-    ('tienda8' ,'estados unidos' , '9531234328', 'deportivo'),
-    ('tienda9' ,'durango' , '9531375209', 'deportivo'),
-    ('tienda10' ,'chiapas' , '95534656987', 'deportivo');
+    INSERT INTO  concesionaria (id_concesionaria, nombre, ubicacion, telefono,tipo_auto)
+    values ('111' ,'tienda1' ,'tlaxiaco' , '9531145698', 'deportivo'),
+    ('112' ,'tienda2' ,'oaxaca' , '9531145698', 'deportivo'),
+    ('113' ,'tienda3' ,'mexico' , '9531178890', 'deportivo'),
+    ('114' ,'tienda4' ,'puebla' , '9531146112', 'deportivo'),
+    ('115' ,'tienda5' ,'tijuana' , '9531143709', 'deportivo'),
+    ('116' ,'tienda6' ,'putla' , '9531180098', 'deportivo'),
+    ('117' ,'tienda7' ,'jalisco' , '9531146458', 'deportivo'),
+    ('118' ,'tienda8' ,'estados unidos' , '9531234328', 'deportivo'),
+    ('119' ,'tienda9' ,'durango' , '9531375209', 'deportivo'),
+    ('110' ,'tienda10' ,'chiapas' , '95534656987', 'deportivo');
     
-INSERT INTO  cliente (
-    nombre,
-    telefono,
-    dereccion,
-    )
-    values ('luis' ,'9534659863' , 'tlaxiaco'),
-    ('jorge' ,'9533769912' , 'oaxaca'),
-    ('kevin' ,'9535479899' , 'mexico'),
-    ('noe' ,'9534685112' , 'putla'),
-    ('bryan' ,'9534321676' , 'huajuapan'),
-    ('fernando' ,'9533245789' , 'tayata'),
-    ('jose' ,'9533446578' , 'tlaxiaco'),
-    ('eric' ,'9533241156' , 'oaxaca'),
-    ('juan' ,'9539907104' , 'huajuapan'),
-     ('leo' ,'9535432123' , 'tlaxiaco');
+INSERT INTO  cliente (id_cliente, nombre, telefono, domicilio)
+    values ('000001' ,'luis' ,'9534659863' , 'tlaxiaco'),
+    ('000002' ,'jorge' ,'9533769912' , 'oaxaca'),
+    ('000003' ,'kevin' ,'9535479899' , 'mexico'),
+    ('000004' ,'noe' ,'9534685112' , 'putla'),
+    ('000005' ,'bryan' ,'9534321676' , 'huajuapan'),
+    ('000006' ,'fernando' ,'9533245789' , 'tayata'),
+    ('000007' ,'jose' ,'9533446578' , 'tlaxiaco'),
+    ('000008' ,'eric' ,'9533241156' , 'oaxaca'),
+    ('000009' ,'juan' ,'9539907104' , 'huajuapan'),
+     ('000010' , 'leo' ,'9535432123' , 'tlaxiaco');
      
-    INSERT INTO  empleado (
-    nombre,
-    telefono,
-    correo_electronico,
-    )
-    values ('pepe' ,'9535577689' , 'pepe@gmail.com'),
-    ('emanuel' ,'9534654533' , 'ema@gmail.com'),
-    ('fer' ,'9534611233' , 'fer@gmail.com'),
-    ('kevin' ,'95346556763' , 'kevin@gmail.com'),
-    ('sofia' ,'953463456' , 'sofi@gmail.com'),
-    ('pablo' ,'9534653355' , 'pag@gmail.com'),
-    ('mario' ,'9534332211' , 'ma@gmail.com'),
-    ('luis' ,'9530099643' , 'luis@gmail.com'),
-    ('antonio' ,'9534634409' , 'antonio@gmail.com'),
-    ('leonel' ,'9534645900' , 'leo@gmail.com');
+    INSERT INTO  empleado (id_empleado, nombre,telefono,correo_electronico)
+    values ('101' ,'pepe' ,'9535577689' , 'pepe@gmail.com'),
+    ('102' ,'emanuel' ,'9534654533' , 'ema@gmail.com'),
+    ('103' ,'fer' ,'9534611233' , 'fer@gmail.com'),
+    ('104' ,'kevin' ,'95346556763' , 'kevin@gmail.com'),
+    ('105' ,'sofia' ,'953463456' , 'sofi@gmail.com'),
+    ('106' ,'pablo' ,'9534653355' , 'pag@gmail.com'),
+    ('107' ,'mario' ,'9534332211' , 'ma@gmail.com'),
+    ('108' ,'luis' ,'9530099643' , 'luis@gmail.com'),
+    ('109' ,'antonio' ,'9534634409' , 'antonio@gmail.com'),
+    ('110' ,'leonel' ,'9534645900' , 'leo@gmail.com');
